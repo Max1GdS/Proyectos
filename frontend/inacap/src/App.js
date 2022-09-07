ceb83ecd1ad274ff649df992e9fd39bcb307c328
Max1GdS <elmaximilianog@gmail.com>
2022-09-07 17:22:05 -0300

    Inicios React

---
+++ frontend/inacap/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'antd/dist/antd.min.css'
+import Plantillas from './features/plantilla/Plantillas'
+
+function App() {
+  return (
+    <div>
+      <Plantillas/>
+
+    </div>
+  );
+}
+
+export default App;
